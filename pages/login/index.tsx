229be727cd432a09506be53c109dcd9bc17689fa
Gutemberg Lamark <gtmbrglamark@gmail.com>
2022-11-19 08:21:10 -0300

    feat: adicionando framer motion nas paginas para suavizacao

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,13 +1,21 @@
+import { motion } from "framer-motion";
+
 import FormLogin from "../../components/Forms/FormLogin";
+
 import { Main } from "../../styles/Login/style";
 
 const Login = () => {
   return (
     <Main>
-      <section>
+      <motion.section
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.5 }}
+      >
         <h1>Login</h1>
         <FormLogin />
-      </section>
+      </motion.section>
     </Main>
   );
 };
